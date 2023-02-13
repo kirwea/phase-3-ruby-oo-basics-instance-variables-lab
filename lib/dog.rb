6d9fbe753ab39407378f273bac1f8270f25fe940
dennistaiti <dennistaiti7@gmail.com>
2023-02-13 21:13:48 +0300

    complete

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
-
-end
+    def name=(dog_name)
+      @this_dogs_name = dog_name
+    end
+  
+    def name
+      @this_dogs_name
+    end
+  end
+  
+  kush = Dog.new
+  kush.name = "kush"
+  
+  puts kush.name 
+  
